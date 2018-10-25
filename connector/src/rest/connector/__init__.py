d577a50d75bb949833422b5dcb9d319827863f5c
Siming Yuan <siyuan@cisco.com>
2018-10-25 17:03:39 -0400

    update to ces code

--- connector/src/rest/connector/__init__.py
+++ connector/src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 __author__ = 'Jean-Benoit Aubin <jeaubin@cisco.com>'
 __contact__ = 'pyats-support@cisco.com'
 __copyright__ = 'Cisco Systems, Inc. Cisco Confidential'
@@ -14,7 +14,25 @@ from ats.connections import BaseConnection
 from abstract import Lookup
 import rest.connector.libs
 
-# create a logger for this module
+# try to record usage statistics
+#  - only internal cisco users will have stats.CesMonitor module
+#  - below code does nothing for DevNet users -  we DO NOT track usage stats
+#    for PyPI/public/customer users
+try:
+    # new internal cisco-only pkg since devnet release
+    from ats.cisco.stats import CesMonitor
+except Exception:
+    try:
+        # legacy pyats version, stats was inside utils module
+        from ats.utils.stats import CesMonitor
+    except Exception:
+        CesMonitor = None
+
+finally:
+    if CesMonitor is not None:
+        # CesMonitor exists -> this is an internal cisco user
+        CesMonitor(action = __name__, application='pyATS Packages').post()
+
 
 
 class Rest(BaseConnection):
