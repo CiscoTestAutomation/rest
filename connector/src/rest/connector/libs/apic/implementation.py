7ff9ea801d9de712dc1e8ab059099532e462cb3f
Takashi Higashimura <tahigash@cisco.com>
2020-03-11 21:59:22 -0400

    changed logging level

--- connector/src/rest/connector/libs/apic/implementation.py
+++ connector/src/rest/connector/libs/apic/implementation.py
@@ -229,7 +229,7 @@ class Implementation(Implementation):
             full_url += "&rsp-subtree-class={rsc}"\
                 .format(rsc=rsp_subtree_class)
 
-        log.debug("Sending GET command to '{d}':"\
+        log.info("Sending GET command to '{d}':"\
                  "\nDN: {furl}".format(d=self.device.name, furl=full_url))
 
         response = self.session.get(full_url, timeout=timeout, verify=False)
@@ -278,7 +278,7 @@ class Implementation(Implementation):
         # Deal with the dn
         full_url = '{f}{dn}'.format(f=self.url, dn=dn)
 
-        log.debug("Sending POST command to '{d}':"\
+        log.info("Sending POST command to '{d}':"\
                  "\nDN: {furl}\nPayload:{p}".format(d=self.device.name,
                                                     furl=full_url,
                                                     p=payload))
@@ -322,7 +322,7 @@ class Implementation(Implementation):
         # Deal with the dn
         full_url = '{f}{dn}'.format(f=self.url, dn=dn)
 
-        log.debug("Sending DELETE command to '{d}':"\
+        log.info("Sending DELETE command to '{d}':"\
                  "\nDN: {furl}".format(d=self.device.name, furl=full_url))
 
         # Send to the device
