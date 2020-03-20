7ff03150ee7f6fca93de18701bed68fabbff7fc1
Takashi Higashimura <tahigash@cisco.com>
2020-03-20 15:25:18 -0400

    for beta 20.2.1b1

--- connector/src/rest/connector/__init__.py
+++ connector/src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '19.12'
+__version__ = '20.2.1b1'
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
