4f6243e2723e18fb8a2e576b510dabf5deb986dc
Takashi Higashimura <tahigash@cisco.com>
2021-01-03 08:18:31 -0500

    remoted CesMonitor related code

--- connector/src/rest/connector/__init__.py
+++ connector/src/rest/connector/__init__.py
@@ -16,26 +16,6 @@ from pyats.connections import BaseConnection
 from genie.abstract import Lookup
 import rest.connector.libs
 
-# try to record usage statistics
-#  - only internal cisco users will have stats.CesMonitor module
-#  - below code does nothing for DevNet users -  we DO NOT track usage stats
-#    for PyPI/public/customer users
-try:
-    # new internal cisco-only pkg since devnet release
-    from pyats.cisco.stats import CesMonitor
-except Exception:
-    try:
-        # legacy pyats version, stats was inside utils module
-        from pyats.utils.stats import CesMonitor
-    except Exception:
-        CesMonitor = None
-
-finally:
-    if CesMonitor is not None:
-        # CesMonitor exists -> this is an internal cisco user
-        CesMonitor(action = __name__, application='pyATS Packages').post()
-
-
 
 class Rest(BaseConnection):
     '''Rest
