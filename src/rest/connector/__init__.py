cd2d53159baf5454c8503a8fc09055cd29210185
dangrazi <dangrazi@cisco.com>
2021-04-26 21:28:54 -0400

    bumped version to 21.4

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '21.3'
+__version__ = '21.4'
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
