b9233018a1f9575f12cbe30af1add30c9724bc1a
lsheikal <lsheikal@cisco.com>
2022-04-25 18:14:44 -0400

    Releasing v22.4

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '22.3'
+__version__ = '22.4'
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
