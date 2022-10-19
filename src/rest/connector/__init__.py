2e0d3df220578b06b7cf128417e5d97d904bafad
lgerrior <lgerrior@cisco.com>
2022-10-19 14:34:37 -0400

    Bump Version

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '22.9'
+__version__ = '22.10'
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
