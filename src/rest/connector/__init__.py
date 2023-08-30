818ecda126f7bd1815a2c2c26563c2320c8f84bc
Dave Wapstra <dwapstra@cisco.com>
2023-08-30 12:36:28 +1200

    Bump version 23.7 -> 23.8

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = '23.7'
+__version__ = '23.8'
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
