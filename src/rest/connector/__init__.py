1db5e18468d0adbb080d0e2d7a4baae6c31dd117
omid <omehrabi@cisco.com>
2026-01-29 17:14:11 -0500

    Open branches for release_26.1

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = "25.11"
+__version__ = "26.1"
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
