fd8678174b2908623bf8f6cd68a1e96a1d977102
Taarini Sarath Chander <tasarath@cisco.com>
2026-08-05 16:06:09 -0700

    Open branches for release_26.7

--- src/rest/connector/__init__.py
+++ src/rest/connector/__init__.py
@@ -2,7 +2,7 @@
 the device via REST api"""
 
 # metadata
-__version__ = "26.6"
+__version__ = "26.7"
 __author__ = ['Jean-Benoit Aubin <jeaubin@cisco.com>',
               'Takashi Higashimura (tahigash) <tahigash@cisco.com>']
 
